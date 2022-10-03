b069b49b33a6ea33d6effc6544fa582cd70c0206
Evan <94895335+WarLay0@users.noreply.github.com>
2022-10-03 11:57:02 +0200

    :tada: | Création du fichier /style.css/

--- index.php
+++ index.php
@@ -5,8 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Evan BOMBART - PortFolio en ligne</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <nav>
+        <ul>
+            <li><a href="#accueil">Accueil</a></li>
+            <li><a href="#">Présentation</a></li>
+            <li><a href="#">Projets</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+    <section id="accueil">
+        <div>
+            <h1>Evan BOMBART</h1>
+            <h2>Développeur full stack junior</h2>
+        </div>
+    </section>
     
 </body>
 </html>
