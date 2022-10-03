36914cbd451dc5b9d96ea14ec42871de1938c382
Evan <94895335+WarLay0@users.noreply.github.com>
2022-10-03 16:24:01 +0200

    :sparkles: | Ajout d'une section /presentation/

--- index.php
+++ index.php
@@ -16,12 +16,24 @@
             <li><a href="#">Contact</a></li>
         </ul>
     </nav>
+    <!-- Barre de navigation -->
     <section id="accueil">
         <div>
             <h1>Evan BOMBART</h1>
             <h2>Développeur full stack junior</h2>
         </div>
     </section>
-    
+    <!-- Accueil -->
+    <section id="presentation">
+        <div id="pre_under">
+            <img src="img/evanPhoto.png" alt="Photo Evan">
+            <div>
+                <h2>Présentation</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod nobis vel molestiae facilis! Eos ullam suscipit quam velit quibusdam esse, aliquam dicta commodi, ratione tenetur, maxime provident nam inventore officiis.</p>
+            </div>
+        </div>
+        <a href="#"><button>Curiculum Vitae</button></a>
+    </section>    
+    <!-- Présentation -->
 </body>
 </html>
